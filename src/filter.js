dd52371b15bc2f3636a229714b44f8236de89fb9
Stefan Stölzle <stefan@stoelzle.me>
2017-01-17 16:25:16 +0100

    Remove lodash dependency

--- src/filter.js
+++ src/filter.js
@@ -1,15 +1,16 @@
 #!/usr/local/bin/node
 
 const path = require('path');
-const _ = require('lodash');
-const workflows = require('../workflows.json');
+const workflows = require('../workflows.json').items;
 
 let items = [];
 let substring = process.argv[2];
 let regex = new RegExp(substring, 'i');
 
-_.forIn(workflows.items, flow => {
-  _.merge(flow, {
+for (var i = 0; i < workflows.length; i++) {
+  let flow = workflows[i];
+
+  Object.assign(flow, {
     arg: flow.uid,
     valid: regex.test(flow.uid),
     autocomplete: flow.uid,
@@ -20,15 +21,11 @@ _.forIn(workflows.items, flow => {
       path: path.resolve(__dirname, '../assets/' + flow.icon + '.png')
     } : null
   });
-});
 
-_.pickBy(workflows.items, flow => {
   if (regex.test(flow.uid) || (flow.cmd.indexOf(substring) > -1)) {
     items.push(flow);
-
-    return flow;
   }
-});
+}
 
 console.log(JSON.stringify({
   items: items
